47a0f12a007eae97154acb75890b8acd5a74ceb4
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2021-08-17 13:15:34 +0100

    added a main.py app

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.quora.com/')
+
+emailField = driver.find_element_by_xpath('//*[@id="email"]')
+emailField.send_keys('crixadsinfo@gmail.com')
+
+passwordField = driver.find_element_by_xpath('//*[@id="password"]')
+passwordField.send_keys('crixADS001')
+
+
+button = driver.find_element_by_xpath('//*[@id="root"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[4]/button/div/div/div')
+
+button.click()
+
