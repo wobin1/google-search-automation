203376f06191626f9f4d421e359eecf5a89b49a0
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2021-09-04 13:31:57 +0100

    fixed gif

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ emailField = driver.find_element_by_xpath('//*[@id="email"]')
 emailField.send_keys('crixadsinfo@gmail.com')
 
 passwordField = driver.find_element_by_xpath('//*[@id="password"]')
-passwordField.send_keys('crixADS001')
+passwordField.send_keys('fakepassword')
 
 
 button = driver.find_element_by_xpath('//*[@id="root"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[4]/button/div/div/div')
